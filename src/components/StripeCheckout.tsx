2b2514fd09fdbb41d491e612cdfb0c25c83acd46
ArtyomZakharenko <artiomzaharenko@gmail.com>
2022-10-01 17:02:02 +0300

    Completed checkout

--- src/components/StripeCheckout.tsx
+++ src/components/StripeCheckout.tsx
@@ -1,39 +1,164 @@
-import { useState, useEffect } from 'react'
 import styled from 'styled-components'
-import { loadStripe } from '@stripe/stripe-js'
+import { loadStripe, StripeCardElementChangeEvent } from '@stripe/stripe-js'
 import {
-  CardElement,
-  useStripe,
-  Elements,
-  useElements,
-} from '@stripe/react-stripe-js'
-import axios from 'axios'
-import { useCartContext } from '../context/cart_context'
-import { useUserContext } from '../context/user_context'
-import { formatPrice } from '../utils/helpers'
+	CardElement,
+	useStripe,
+	Elements,
+	useElements,
+} from '@stripe/react-stripe-js';
+import { useCartContext } from "../context/cart_context";
+import { useUserContext } from "../context/user_context";
+import { useNavigate } from "react-router-dom";
+import { FormEvent, useEffect, useState } from "react";
+import axios from "axios";
+import { formatPrice } from "../utils/helpers";
+
+
+const promise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
 const CheckoutForm = () => {
-  return <h4>hello from Stripe Checkout </h4>
+	const { cart, total_amount, shipping_fee, clearCart } = useCartContext();
+	const { myUser } = useUserContext();
+	const navigate = useNavigate();
+	// stripe
+	const [succeeded, setSucceeded] = useState(false);
+	const [error, setError] = useState(null);
+	const [processing, setProcessing] = useState(false);
+	const [disabled, setDisabled] = useState(true);
+	const [clientSecret, setClientSecret] = useState('');
+	const stripe = useStripe();
+	const elements = useElements();
+
+	const cardStyle = {
+		style: {
+			base: {
+				color: '#32325d',
+				fontFamily: 'Arial, sans-serif',
+				fontSmoothing: 'antialiased',
+				fontSize: '16px',
+				'::placeholder': {
+					color: '#32325d',
+				},
+			},
+			invalid: {
+				color: '#fa755a',
+				iconColor: '#fa755a',
+			},
+		},
+	};
+
+	const createPaymentIntent = async () => {
+		try {
+			const { data } = await axios.post(
+				'/.netlify/functions/create-payment-intent',
+				JSON.stringify({ cart, shipping_fee, total_amount })
+			);
+			setClientSecret(data.clientSecret);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	useEffect(() => {
+		createPaymentIntent();
+	}, []);
+
+
+	const handleChange = async (event: StripeCardElementChangeEvent) => {
+		// Listen for changes in the CardElement
+		// and display any errors as the customer types their card details
+		setDisabled(event.empty);
+		setError(event.error ? event.error.message : '');
+	};
+
+	const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		setProcessing(true);
+		const payload = await stripe.confirmCardPayment(clientSecret, {
+			payment_method: {
+				card: elements.getElement(CardElement),
+			},
+		});
+		if (payload.error) {
+			setError(`Payment failed ${payload.error.message}`);
+			setProcessing(false);
+		} else {
+			setError(null);
+			setProcessing(false);
+			setSucceeded(true);
+			setTimeout(() => {
+				clearCart();
+				navigate('/');
+			}, 5000);
+		}
+	};
+
+	return (
+		<div>
+			{succeeded ? (
+				<article>
+					<h4>Thank you</h4>
+					<h4>Your payment was successful!</h4>
+					<h4>Redirecting to home page shortly</h4>
+				</article>
+			) : (
+				<article>
+					<h4>Hello, {myUser && myUser.name}</h4>
+					<p>Your total is {formatPrice(total_amount)}</p>
+					<p>Test Card Number: 4242 4242 4242 4242</p>
+				</article>
+			)}
+			<form id='payment-form' onSubmit={handleSubmit}>
+				<CardElement
+					id='card-element'
+					options={cardStyle}
+					onChange={handleChange}
+				/>
+				<button disabled={processing || disabled || succeeded} id='submit'>
+          <span id='button-text'>
+            {processing ? <div className='spinner' id='spinner'></div> : 'Pay'}
+          </span>
+				</button>
+				{/* Show any error that happens when processing the payment */}
+				{error && (
+					<div className='card-error' role='alert'>
+						{error}
+					</div>
+				)}
+				{/* Show a success message upon completion */}
+				<p className={succeeded ? 'result-message' : 'result-message hidden'}>
+					Payment succeeded, see the result in your
+					<a href={`https://dashboard.stripe.com/test/payments`}>
+						{' '}
+						Stripe dashboard.
+					</a>{' '}
+					Refresh the page to pay again.
+				</p>
+			</form>
+		</div>
+	);
 }
 
 const StripeCheckout = () => {
-  return (
-    <Wrapper>
-      <CheckoutForm />
-    </Wrapper>
-  )
+	return (
+		<Wrapper>
+			<Elements stripe={promise}>
+				<CheckoutForm/>
+			</Elements>
+		</Wrapper>
+	)
 }
 
 const Wrapper = styled.section`
   form {
     width: 30vw;
     align-self: center;
-    box-shadow: 0px 0px 0px 0.5px rgba(50, 50, 93, 0.1),
-      0px 2px 5px 0px rgba(50, 50, 93, 0.1),
-      0px 1px 1.5px 0px rgba(0, 0, 0, 0.07);
+    box-shadow: 0 0 0 0.5px rgba(50, 50, 93, 0.1),
+    0 2px 5px 0 rgba(50, 50, 93, 0.1),
+    0 1px 1.5px 0 rgba(0, 0, 0, 0.07);
     border-radius: 7px;
     padding: 40px;
   }
+
   input {
     border-radius: 6px;
     margin-bottom: 6px;
@@ -45,18 +170,22 @@ const Wrapper = styled.section`
     background: white;
     box-sizing: border-box;
   }
+
   .result-message {
     line-height: 22px;
     font-size: 16px;
   }
+
   .result-message a {
     color: rgb(89, 111, 214);
     font-weight: 600;
     text-decoration: none;
   }
+
   .hidden {
     display: none;
   }
+
   #card-error {
     color: rgb(105, 115, 134);
     font-size: 16px;
@@ -64,6 +193,7 @@ const Wrapper = styled.section`
     margin-top: 12px;
     text-align: center;
   }
+
   #card-element {
     border-radius: 4px 4px 0 0;
     padding: 12px;
@@ -73,10 +203,13 @@ const Wrapper = styled.section`
     background: white;
     box-sizing: border-box;
   }
+
   #payment-request-button {
     margin-bottom: 32px;
   }
+
   /* Buttons and links */
+
   button {
     background: #5469d4;
     font-family: Arial, sans-serif;
@@ -89,40 +222,47 @@ const Wrapper = styled.section`
     cursor: pointer;
     display: block;
     transition: all 0.2s ease;
-    box-shadow: 0px 4px 5.5px 0px rgba(0, 0, 0, 0.07);
+    box-shadow: 0 4px 5.5px 0 rgba(0, 0, 0, 0.07);
     width: 100%;
   }
+
   button:hover {
     filter: contrast(115%);
   }
+
   button:disabled {
     opacity: 0.5;
     cursor: default;
   }
+
   /* spinner/processing state, errors */
+
   .spinner,
   .spinner:before,
   .spinner:after {
     border-radius: 50%;
   }
+
   .spinner {
     color: #ffffff;
     font-size: 22px;
     text-indent: -99999px;
-    margin: 0px auto;
+    margin: 0 auto;
     position: relative;
     width: 20px;
     height: 20px;
     box-shadow: inset 0 0 0 2px;
-    -webkit-transform: translateZ(0);
+    --webkit-transform: translateZ(0);
     -ms-transform: translateZ(0);
     transform: translateZ(0);
   }
+
   .spinner:before,
   .spinner:after {
     position: absolute;
     content: '';
   }
+
   .spinner:before {
     width: 10.4px;
     height: 20.4px;
@@ -135,6 +275,7 @@ const Wrapper = styled.section`
     -webkit-animation: loading 2s infinite ease 1.5s;
     animation: loading 2s infinite ease 1.5s;
   }
+
   .spinner:after {
     width: 10.4px;
     height: 10.2px;
@@ -142,18 +283,19 @@ const Wrapper = styled.section`
     border-radius: 0 10.2px 10.2px 0;
     top: -0.1px;
     left: 10.2px;
-    -webkit-transform-origin: 0px 10.2px;
-    transform-origin: 0px 10.2px;
+    -webkit-transform-origin: 0 10.2px;
+    transform-origin: 0 10.2px;
     -webkit-animation: loading 2s infinite ease;
     animation: loading 2s infinite ease;
   }
+
   @keyframes loading {
     0% {
-      -webkit-transform: rotate(0deg);
+      --webkit-transform: rotate(0deg);
       transform: rotate(0deg);
     }
     100% {
-      -webkit-transform: rotate(360deg);
+      --webkit-transform: rotate(360deg);
       transform: rotate(360deg);
     }
   }
--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -4,18 +4,27 @@ import { useCartContext } from '../context/cart_context'
 import { Link } from 'react-router-dom'
 
 const CheckoutPage = () => {
+  const { cart } = useCartContext();
+
   return (
     <main>
       <PageHeader title='checkout' />
       <Wrapper className='page'>
-        <div className='section-center page'>
-          <h3>checkout</h3>
-        </div>
+        {!cart.length ? (
+          <div className='empty'>
+            <h2>Your cart is empty</h2>
+            <Link to='/products' className='btn'>
+              fill it
+            </Link>
+          </div>
+        ) : (
+          <StripeCheckout />
+        )}
       </Wrapper>
     </main>
-
-  );
+  )
 }
+
 const Wrapper = styled.div`
   display: flex;
   justify-content: center;
